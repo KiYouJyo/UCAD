efe51245a79d6aed82907c6559a8f9d6f244fb32
Kiyō Jo <x2425618950@gmail.com>
2026-08-18 11:13:14 +0800

    v0.12: initialize viewport manager

--- src/UCAD.App/App.xaml.cs
+++ src/UCAD.App/App.xaml.cs
@@ -39,6 +39,7 @@ public partial class App : Application
             mainWindow.EnsureAdvancedAuthoringUiInitialized();
             mainWindow.EnsureAdvancedAuthoringToolbarBridgeInitialized();
             mainWindow.EnsureLayoutPlotUiInitialized();
+            mainWindow.EnsureViewportManagerUiInitialized();
             mainWindow.EnsureLayoutPersistenceBridgeInitialized();
             mainWindow.EnsureFileUiInitialized();
             mainWindow.EnsureRecentFilesInitialized();
